a621ed95049169c1414866c07cb9f3b065149f7f
Andrew M Westling & Katharine Chen <student@epicodus.com>
2014-09-11 14:44:39 -0700

    add response.score method to calculate a response's score based on votes

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,8 +2,17 @@ class Response < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :question
+  has_many :votes
 
   validates :response, :presence => true
   validates :user_id, :presence => true
+
+  def score
+    score = 0
+    self.votes.each do | vote |
+      score += vote.value
+    end
+    score
+  end
 
 end
--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -5,4 +5,18 @@ describe Response do
   it { should belong_to :question}
   it { should validate_presence_of :response}
   it { should validate_presence_of :user_id}
+  it { should have_many :votes }
+
+  it "should add up all the votes for a specific response" do
+    user = User.create({full_name: "Test User", email: "omg@aol.com", password:
+    "go"})
+    question = Question.create({question: "What time is it?", user_id: "#{user.id}", content: "I can't see the clock"})
+    response = Response.create({response: "Don't be lazy, look at a clock ugh", user_id: "#{user.id}", question_id: "#{question.id}"})
+    voteup = Vote.create({value: "1", response_id: "#{response.id}", user_id: "#{user.id}"})
+    votedown = Vote.create({value: "-1", response_id: "#{response.id}", user_id: "#{user.id}"})
+    voteup1 = Vote.create({value: "1", response_id: "#{response.id}", user_id: "#{user.id}"})
+    voteup2 = Vote.create({value: "1", response_id: "#{response.id}", user_id: "#{user.id}"})
+    expect(response.score).to eq 2
+
+  end
 end
